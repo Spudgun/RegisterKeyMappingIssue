b0e0aa2b3d0d609bf9cfdbad6a1f8527167d6201
Spudgun <jared.rj.white@gmail.com>
2020-09-25 01:12:07 +1200

    Example Script

---
+++ template/client/cl_main.lua
@@ -0,0 +1,5 @@
+RegisterCommand("fooBar", function()
+    print("Hello World")
+end, false)
+
+RegisterKeyMapping("fooBar", "Foobar", "keyboard", "k")
---
+++ template/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "bodacious"
+games { "gta5" }
+
+author "Author"
+description "Resource Description"
+version "1.0.0"
+
+shared_scripts {
+    "**/sh_*.lua",
+}
+
+client_scripts {
+    "**/cl_*.lua",
+}
+
+server_scripts {
+    "**/sv_*.lua",
+}
